bf79d90fc3625e9242ea077b69e06608144469de
musaubrian <musaubrian15@gmail.com>
2024-05-30 23:41:36 +0300

    feat: add alot
    - group results by parent dir
    - add utilities to manipulate the displayed text
    - store different indices for different dirs
    - load diff indices based on choice

--- sik.py
+++ sik.py
@@ -5,6 +5,8 @@ import re
 import json
 import argparse
 
+INDICES_DIR = "./indices"
+
 
 def read_markdown_files(directory):
     files_content = {}
@@ -87,6 +89,8 @@ def highlight_query_in_line(query, line):
 def search_kb(query, index_path):
     indices = load_indices(index_path)
     results = search_indices(query, indices)
+    if not results:
+        print(f"Found nothing matching {print_bold(query)}")
 
     grouped_results = {}
     for filename, locations in results.items():
@@ -98,14 +102,15 @@ def search_kb(query, index_path):
             content = indices[file_path]['content']
             line = content.split('\n')[line_no - 1]
             highlighted_line = highlight_query_in_line(query, line)
-            location = f"{os.path.basename(file_path)}:{line_no},{col_no}"
+            location = f"{os.path.basename(file_path)}:{line_no}"
             grouped_results[parent_dir].append((location, highlighted_line))
 
     for parent_dir, file_results in grouped_results.items():
-        print(print_bold(parent_dir))
+        print(print_bold(darken(parent_dir)))
         for location, highlighted_line in file_results:
-            print(f"   {print_bold(location)}")
-            print(f"       {highlighted_line}")
+            print(
+                f"   {darken(print_bold(location), level=2)}  "
+                f"  {darken(highlighted_line.strip(), level=4)}")
         print()
 
 
@@ -117,7 +122,58 @@ def highlight(text):
     return f"\033[38;5;208m{text}\033[0m"
 
 
+def darken(text, level=1):
+    # '#f4f4f4'
+    base_r, base_g, base_b = 244, 244, 244
+    darken_amount = 15
+    # Calculate the new RGB values
+    r = max(0, base_r - level * darken_amount)
+    g = max(0, base_g - level * darken_amount)
+    b = max(0, base_b - level * darken_amount)
+
+    return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+
+
+def get_index_file(directory):
+    dir_name = os.path.basename(os.path.normpath(directory))
+    return f"{dir_name}_index.sik"
+
+
+def list_available_indices():
+    return [file for file in os.listdir(INDICES_DIR) if file.endswith('_index.sik')]
+
+
+def create_indices_dir():
+    if not os.path.exists(INDICES_DIR):
+        os.makedirs(INDICES_DIR)
+        print(print_bold(darken(f"Created {INDICES_DIR}", level=5)))
+
+
+def select_index_file():
+    available_indices = list_available_indices()
+    if not available_indices:
+        print(darken("No index files found. Please create an index first."))
+        return None
+
+    if len(available_indices) == 1:
+        return available_indices[0]
+
+    print(print_bold("Available indices:"))
+    print(darken("=" * 30))
+    for idx, file in enumerate(available_indices, 1):
+        print(f"{darken(f'{idx:2d}:')} {print_bold(file)}")
+    print(darken("=" * 30))
+
+    choice = int(input(darken("Select the index file by number: "))) - 1
+    if 0 <= choice < len(available_indices):
+        return available_indices[choice]
+    else:
+        print(darken("Invalid selection."))
+        return None
+
+
 def main():
+    create_indices_dir()
     parser = argparse.ArgumentParser(
         description="Search markdown files for a query.")
     parser.add_argument(
@@ -129,14 +185,20 @@ def main():
 
     args = parser.parse_args()
     directory = args.dir
-    json_index_path = 'index.sik'
 
     if args.index:
+        if directory is None:
+            print("Please specify a directory to index.")
+            return
+        json_index_path = f"{INDICES_DIR}/{get_index_file(directory)}"
         print("Indexing files...")
         indices = create_indices_for_directory(directory)
         store_indices(indices, json_index_path)
-        print("Indexing complete.")
+        print(f"Created index: {print_bold(json_index_path)}")
     elif args.query:
+        json_index_path = f"{INDICES_DIR}/{select_index_file()}"
+        if json_index_path is None:
+            return
         query = args.query
         search_kb(query, json_index_path)
     else:
