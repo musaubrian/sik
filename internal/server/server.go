f0af3c245bcb1188404ba79cc7260de7638ac64a
Ernest <me@nabri.xyz>
2024-10-20 10:59:35 +0300

    feat: open up file in browser (#3)
    * feat: open up file in browser
    
    * update readme
    
    * feat: use local markd js
    
    - Serve marked js with the rest of it, no need to make a network request
    
    * update readme

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,20 +1,23 @@
 package server
 
 import (
-	_ "embed"
+	"bufio"
+	"embed"
 	"encoding/json"
 	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/musaubrian/sik/internal/core"
 	"github.com/musaubrian/sik/internal/engine"
 	"github.com/musaubrian/sik/internal/utils"
 )
 
-//go:embed html/index.html
-var html []byte
+//go:embed www/*
+var www embed.FS
 
 type Server struct {
 	port   string
@@ -34,11 +37,40 @@ func (s *Server) WithPort(port string) *Server {
 }
 
 func (s *Server) Start() {
+
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(html)
+		indexHtml, err := www.ReadFile("www/index.html")
+		if err != nil {
+			slog.Error(fmt.Sprintf("[/] Failed to get index.html: %v", err))
+			http.Error(w, "Unable to get page", http.StatusInternalServerError)
+			return
+		}
+		w.Write(indexHtml)
 	})
+	http.HandleFunc("GET /doc", func(w http.ResponseWriter, r *http.Request) {
+		markdHtml, err := www.ReadFile("www/markd.html")
+		if err != nil {
+			slog.Error(fmt.Sprintf("[/doc] Failed to get markd.html: %v", err))
+			http.Error(w, "Unable to get page", http.StatusInternalServerError)
+			return
+		}
+		w.Write(markdHtml)
+	})
+
+	http.HandleFunc("GET /js/markd.min.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json;charset=utf-8")
+		markMinJs, err := www.ReadFile("www/markd.min.js")
+		if err != nil {
+			slog.Error(fmt.Sprintf("[/markd.min.js] Failed to get markdjs: %v", err))
+			http.Error(w, "Unable to get js asset", http.StatusInternalServerError)
+			return
+		}
+		w.Write(markMinJs)
+	})
+
 	http.HandleFunc("POST /search", s.handleSearch)
 	http.HandleFunc("GET /reload", s.handleReload)
+	http.HandleFunc("GET /read-doc", s.handleReadDoc)
 
 	slog.Info(fmt.Sprintf("Server running at localhost:%s", s.port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", s.port), nil))
@@ -85,4 +117,38 @@ func (s *Server) handleReload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	s.engine = engine.New(index)
+}
+
+func (s *Server) handleReadDoc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	fileurl := r.URL.Query().Get("url")
+	f, err := os.Open(fileurl)
+	if err != nil {
+		msg := fmt.Errorf("Failed to open file: %v", err)
+		slog.Error(msg.Error())
+		http.Error(w, msg.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	var content strings.Builder
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		content.WriteString(scanner.Text() + "\n")
+	}
+	if err := scanner.Err(); err != nil {
+		msg := fmt.Errorf("Failed to read Files contents: %v", err)
+		slog.Error(msg.Error())
+		http.Error(w, msg.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(content.String())
+
+	if err != nil {
+		http.Error(w, "Unable to encode response", http.StatusInternalServerError)
+		return
+	}
+
 }
